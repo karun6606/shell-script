778f0e8811846f9a0651641f2d68280c8da39fac
karun6606 <karun6606@gmail.com>
2024-07-09 17:46:33 +0530

    variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=Ramesh
+person2=Sachin
+
+echo "$person1: Hi ${person2} How are you?"
+echo "${person2}: Hi $person1 I am good"
+echo "$person1: How are you, How is your work"
+echo "${person2}: not bad, I am planning to upgrade to Devops"
