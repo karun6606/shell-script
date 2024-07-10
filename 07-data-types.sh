34a5059d3045f7afb5c8370df621dea1d0c443c8
karun6606 <karun6606@gmail.com>
2024-07-10 11:03:19 +0530

    datatypes.sh

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-No1=$1
-No2=$2
+# No1=$1
+# No2=$2
 
-sum=$(($No1 + $No2))
+# sum=$(($No1 + $No2))
 
-echo "Total Sum is : $sum"
+# echo "Total Sum is : $sum"
+
+
+NO1=$1
+NO2=$2
+
+SUM=$(($NO1+$NO2))
+
+echo "Total of $NO1 and $NO2 is: $SUM"
