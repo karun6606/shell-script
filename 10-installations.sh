3d40559bc86b2a9c43e60c6740fa856095d4e064
karun6606 <karun6606@gmail.com>
2024-07-10 12:21:13 +0530

    installations.sh

--- 10-installations.sh
+++ 10-installations.sh
@@ -4,10 +4,25 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-echo "Please run with super user"
-exit 1
+    echo "Please run with super user"
+    exit 1
 else
-echo "you are super user"
+    echo "you are super user"
 fi
 
 dnf install mysql -y 
+if [$? -ne 0]
+then
+    echo "Installation of mysql.... failure"
+    exit 1
+else 
+    echo "Installation of mysql.... sucess"
+fi
+
+dnf install tree -y
+if [ $? -ne 0 ]
+then
+    echo "Installation of tree.... failure"
+else
+    echo "Installation of tree... sucess"
+fi
