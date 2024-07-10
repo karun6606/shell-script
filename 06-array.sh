08d2a581813418e4f1c8f9d01e34be1a30285e33
karun6606 <karun6606@gmail.com>
2024-07-10 10:37:25 +0530

    array.sh

--- 06-array.sh
+++ 06-array.sh
@@ -5,5 +5,7 @@ fruits=("Apple" "Orange" "Mango" "Grapes")
 echo "${fruits[0]}"
 echo "${fruits[1]}"
 echo "${fruits[@]}"
+echo "${fruits[*]}"
 echo "${fruits[@]:1}"
 echo "${fruits[@]:2:3}"
+echo "${#fruits[@]}"
