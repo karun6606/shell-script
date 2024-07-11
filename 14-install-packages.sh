8951e98fca2141dc77572c570627c44e1de45699
karun6606 <karun6606@gmail.com>
2024-07-11 12:44:40 +0530

    install-packages.sh

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,17 +5,6 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1F}')
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-VALIDATE() {
-    if [ if $1 -ne 0 ]
-    then
-        echo "$2....Failure"
-        exit 1
-    
-    else
-        echo "$2....Sucess"
-    fi
-}
-
 if [ $userid -ne 0 ]
 then    
     echo "Run with super user"
@@ -24,6 +13,16 @@ else
     echo "You are super user"
 fi
 
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....Failure"
+        exit 1
+    
+    else
+        echo "$2....Sucess"
+    fi
+}
 
 dnf install mysql1 -y &>>$LOGFILE
 VALIDATE $? "Installaton of mysql"
