7a3f67476b43d114c69337cc157db2e74186e9cc
karun6606 <karun6606@gmail.com>
2024-07-16 11:29:05 +0530

    shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,9 +5,9 @@ course=Devops
 echo "Before calling other script, course: $course"
 echo "PID of current script:$$"
 
-./16-other-script.sh 
+#./16-other-script.sh 
 
-#source ./16-other-script.sh
+source ./16-other-script.sh
 
 echo "Afetr calling other script:$course"
 
