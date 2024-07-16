e5777e6c41e1c8c23fac82cec00b2fcb264359ca
karun6606 <karun6606@gmail.com>
2024-07-16 11:07:23 +0530

    shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,9 @@ Course=Devops
 echo "calling before script:$Course"
 echo "PID of current script:$$"
 
-./16-other-script.sh 
+#./16-other-script.sh 
+
+source./16-other-script.sh
 
 echo "calling after script:$Course"
 
