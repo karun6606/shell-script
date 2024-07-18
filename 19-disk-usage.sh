d30e2dbdc3ad5a28651cc4ee8608f2a98642ca5c
karun6606 <karun6606@gmail.com>
2024-07-18 16:14:05 +0530

    disk-usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -11,11 +11,11 @@
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESOLD ]
     then
-        MESSAGE+="$FOLDER is more than $DISK_THRESOLD current usage:$USAGE"
+        MESSAGE+="$FOLDER is more than $DISK_THRESOLD current usage:$USAGE /n"
     fi
  done <<< $DISK_USAGE
 
- echo message:$MESSAGE
+ echo -e message:$MESSAGE
 
 
 
