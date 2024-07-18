370d73e70aca16e769329493a48fc7297383a6b3
karun6606 <karun6606@gmail.com>
2024-07-18 12:18:47 +0530

    delete-old-logs

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -7,9 +7,12 @@ if [ -d $SOURCE_DIRECTORY ]
 then
     echo "Source directory exits"
 else
-    mkdir /tmp/app-logs
+    echo "Please create $SOURCE_DIRECTORY"
+    exit 1
 fi
 
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 echo "$FILES"
+
+rm -rf /tmp/app-logs "$FILES"
